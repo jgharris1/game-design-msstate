2bcb5f2e000ff9d54a6b9916e3429c00365958a9
mtswip <92600965+mtswip@users.noreply.github.com>
2024-02-22 13:28:45 -0600

    Added collision detection and inactive code for damaging enemies

--- Assets/BulletMoveScript.cs
+++ Assets/BulletMoveScript.cs
@@ -21,6 +21,15 @@ public class BulletMoveScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
         Selfdata.targetPos.Set((transform.position.x + bulletDir.x) * speed, (transform.position.y + bulletDir.y) * speed, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Enemy")//will need to be an enemy tag if multiple enemies
+        {
+            //uncomment the below when enemy damage exists
+            //damageApply(enemygameobject, damage, statusId, statusLevel, duration);
+        }
     }
 }
