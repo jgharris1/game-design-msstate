1c346c7fd5b4145d04628b196d7390fb2a51be7a
mtswip <92600965+mtswip@users.noreply.github.com>
2024-03-01 19:56:07 -0600

    Badge start function made

--- Assets/badges/SheriffBadgeScript.cs
+++ Assets/badges/SheriffBadgeScript.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class SheriffBadgeScript : MonoBehaviour
 {
     public entitybasebehavior Selfdata;
-    public Vector3 gavelDir;
+    public Vector3 badgeDir;
     public float speed;
+    public float startPosOffset;
     public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        badgeDir = player.GetComponent<Vector3>();//GetComponent is janky and if there is another Vector3 added to PlayerBaseScript, more specificity will be needed
+
+        if (badgeDir.x == 0.0f)
+        {
+            badgeDir.x += startPosOffset;
+        }
+
+        if (badgeDir.y == 0.0f)
+        {
+            badgeDir.y += startPosOffset;
+        }
     }
 
     // Update is called once per frame
