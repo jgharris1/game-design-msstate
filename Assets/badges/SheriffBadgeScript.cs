104c86451fa2ae6bdd0842071787ef32136fc94f
mtswip <92600965+mtswip@users.noreply.github.com>
2024-03-01 19:38:05 -0600

    Sheriff Badge Created

---
+++ Assets/badges/SheriffBadgeScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheriffBadgeScript : MonoBehaviour
+{
+    public entitybasebehavior Selfdata;
+    public Vector3 gavelDir;
+    public float speed;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
