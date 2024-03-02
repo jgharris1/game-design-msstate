cac48911dc8cd11743f3a1e2d18f1310f5fb7b92
mtswip <92600965+mtswip@users.noreply.github.com>
2024-03-01 19:15:50 -0600

    Aura will track to player movement

--- Assets/auras/IntimidatingAuraScript.cs
+++ Assets/auras/IntimidatingAuraScript.cs
@@ -12,12 +12,16 @@ public class IntimidatingAuraScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        auraDir = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        auraDir = new Vector3 (player.transform.position.x, player.transform.position.y, 0);
+
+        Selfdata.targetPos.Set(transform.position.x + auraDir.x, transform.position.y + auraDir.y, 0);
     }
 }
