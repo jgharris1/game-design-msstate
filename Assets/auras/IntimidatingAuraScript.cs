90cdb4d921fd495104aba26b6681ac34bdd64a2c
mtswip <92600965+mtswip@users.noreply.github.com>
2024-03-01 18:03:54 -0600

    Little more progress on aura

--- Assets/auras/IntimidatingAuraScript.cs
+++ Assets/auras/IntimidatingAuraScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class IntimidatingAuraScript : MonoBehaviour
 {
+    public entitybasebehavior Selfdata;
+    public Vector3 auraDir;
+    private float cooldown = 1.0f;
+    private float timer = 0.0f;
     public GameObject player;
     // Start is called before the first frame update
     void Start()
