8da7b78d3be0d481e1fca34e82654ec44375dab3
mtswip <92600965+mtswip@users.noreply.github.com>
2024-03-01 21:00:52 -0600

    Shotgun made

--- Assets/bullets/shotgun_buckshot/BuckshotScript.cs
+++ Assets/bullets/shotgun_buckshot/BuckshotScript.cs
@@ -28,6 +28,19 @@ public class BuckshotScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x + transform.position.y > 100)//100 is arbitrary, just has to be offscreen
+        {
+            Destroy(gameObject);
+        }
+
+        Selfdata.targetPos.Set((transform.position.x + buckshotDir.x) * speed, (transform.position.y + buckshotDir.y) * speed, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Enemy")//will need to be an enemy tag if multiple enemies
+        {
+            //uncomment the below when enemy damage exists
+            //damageApply(enemygameobject, damage, statusId, statusLevel, duration);
+        }
     }
 }
