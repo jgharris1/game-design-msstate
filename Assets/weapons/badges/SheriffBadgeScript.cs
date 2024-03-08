e35b9cb8d4a5ca6ef7b452ef2050532d9d718683
mtswip <92600965+mtswip@users.noreply.github.com>
2024-03-08 17:49:50 -0600

    Implemented as many badge upgrades as possible

--- Assets/weapons/badges/SheriffBadgeScript.cs
+++ Assets/weapons/badges/SheriffBadgeScript.cs
@@ -7,6 +7,8 @@ public class SheriffBadgeScript : MonoBehaviour
     public Vector3 badgeDir;
     public float speed;
     public float startPosOffset;
+    public int damage;
+    public int level;
     public GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -36,6 +38,50 @@ public class SheriffBadgeScript : MonoBehaviour
 
         transform.position = transform.position + (badgeDir * speed * Time.deltaTime);
     }
+
+    void Upgrade()
+    {
+        level += 1;
+        switch (level)
+        {
+            case 1:
+                speed += 0.1f;
+                break;
+                //need sprite renderer to increase size
+            case 2:
+                break;
+                //need badge spawning in player to implement count increase
+            case 3:
+                player.GetComponent<PlayerBaseScript>().changeFR(1, 800);
+                break;
+            case 4:
+                //another size increase
+                damage += 5;
+                break;
+            case 5:
+                //count increase
+                break;
+            case 6:
+                //count increase
+                speed += .15f;
+                break;
+            case 7:
+                //count increase
+                damage += 5;
+                break;
+            case 8:
+                speed += 0.2f;
+                //size increase
+                break;
+            case 9://infinite upgrade
+                //call for player choice/UI
+                level -= 1;
+                break;
+            default:
+                break;//shouldn't happen; good practice
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")//will need to be an enemy tag if multiple enemies
